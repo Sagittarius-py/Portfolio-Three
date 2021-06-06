e5a187c92403e4c4f9e74248f8f215892ad868e3
Filip <sieniu212@gmail.com>
2021-06-06 14:39:51 +0200

    06

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,19 @@
 import React, { Component } from "react";
+import GitHubIcon from "@material-ui/icons/GitHub";
+
+import { ButtonGroup, Button } from "@material-ui/core";
+
+const buttonStyle = {
+  width: "15vw",
+  height: "15vw",
+  zIndex: "100",
+  borderRadius: "50%",
+  border: "none",
+  background: "none",
+  margin: "0 30px 0 30px",
+};
+
+const itemStyle = { width: "10vw", height: "10vw" };
 
 class Contact extends Component {
   render() {
@@ -7,6 +22,16 @@ class Contact extends Component {
         <div className="Contact content">
           <div className="title contactTitle">
             <h1 id="titleLabel">Contact</h1>
+          </div>
+          <div className="buttonContainer">
+            <ButtonGroup
+              orientation="horizontal"
+              className="ButtonGroupContainer"
+            >
+              <Button variant="contained" className="contactButton">
+                <GitHubIcon style={itemStyle} />
+              </Button>
+            </ButtonGroup>
           </div>
         </div>
       </React.Fragment>
