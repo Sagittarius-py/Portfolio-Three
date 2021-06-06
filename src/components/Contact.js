fcd834e764f36d5d845bc66ceab038875654e7f4
Filip <sieniu212@gmail.com>
2021-06-06 14:46:08 +0200

    06

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,6 @@
 import React, { Component } from "react";
 import GitHubIcon from "@material-ui/icons/GitHub";
-
 import { ButtonGroup, Button } from "@material-ui/core";
-
-const buttonStyle = {
-  width: "15vw",
-  height: "15vw",
-  zIndex: "100",
-  borderRadius: "50%",
-  border: "none",
-  background: "none",
-  margin: "0 30px 0 30px",
-};
 
 const itemStyle = { width: "10vw", height: "10vw" };
 
